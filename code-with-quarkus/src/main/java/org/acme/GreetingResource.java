c1d2864e382329e4c77d52290b12e9195ae5e1cc
Rafael T. C. Soares <rafaelcba@gmail.com>
2024-08-20 17:08:50 -0500

    initial commit

---
+++ code-with-quarkus/src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,17 @@
+package org.acme;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/api")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/hello")
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
+    }
+}
